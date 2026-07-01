c6265229b9255b9011255ea9fbf51fee5c53a683
271313 <jerrymorko2713@proton.me>
2026-06-30 19:58:20 -1200

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,10 @@
-
 // prisma.config.ts
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
-
-if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error("TURSO_DATABASE_URL is missing");
-}
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.TURSO_DATABASE_URL,
+    url: env("DATABASE_URL"),
   },
 });
