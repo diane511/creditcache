0f53d846cf3e55f253749832fd6456d1ae1a1636
271313 <jerrymorko2713@proton.me>
2026-06-30 13:26:02 -1200

    update

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const globalForPrisma = globalThis as unknown as {
   db?: PrismaClient;
 };
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL ?? "file:./prisma/dev.db",
+const adapter = new PrismaLibSql({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 export const db =
